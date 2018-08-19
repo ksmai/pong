e426f9f18d960a06e5f74c3e60596cd06be101bb
ksmai <kasingmai@gmail.com>
2018-08-19 13:01:44 +0800

    Update constants

--- constants.h
+++ constants.h
@@ -1,12 +1,10 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-#include <string>
-
 namespace constants {
-  const int WINDOW_WIDTH = 640;
-  const int WINDOW_HEIGHT = 480;
-  std::string WINDOW_TITLE = "pong";
+  const int WINDOW_WIDTH = 1024;
+  const int WINDOW_HEIGHT = 768;
+  const char *const WINDOW_TITLE = "pong";
 
   const int PADDLE_WIDTH = 100;
   const int PADDLE_HEIGHT = 20;
